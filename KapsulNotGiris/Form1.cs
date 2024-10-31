d60ab5d810d7451edbb53347dfc650c58e342f8a
Abdülsamet YILMAZ <82230018+coderVi@users.noreply.github.com>
2024-11-01 00:52:11 +0300

    Add files via upload

---
+++ KapsulNotGiris/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KapsulNotGiris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci o = new Ogrenci();
+            List<Ogrenci> list = new List<Ogrenci>();
+            o.OgrName = textBox2.Text;
+            o.OgrSurname = textBox3.Text;
+            o.OgrNo = Convert.ToInt32(textBox1.Text);
+
+            o.Not1 = Convert.ToInt32(textBox4.Text);
+            o.Not2 = Convert.ToInt32(textBox5.Text);
+            o.Not3 = Convert.ToInt32(textBox6.Text);
+
+            list.Add(o);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = list;
+        }
+    }
+}
---
+++ KapsulNotGiris/Ogrenci.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsulNotGiris
+{
+    internal class Ogrenci
+    {
+        public Ogrenci() 
+        {
+            not1 = 55;
+            not2 = 60;
+            not3 = 70;
+
+        }
+        int ogrNo;
+        string ogrName;
+        string ogrSurname;
+
+        double not1;
+        double not2;
+        double not3;
+
+        public int OgrNo { get => ogrNo; set => ogrNo = Math.Abs(value); }
+        public string OgrName { get => ogrName; set => ogrName = value.ToUpper(); }
+        public string OgrSurname { get => ogrSurname; set => ogrSurname = value.ToUpper(); }
+
+        public double Not1 { get => not1; 
+            set {
+                if(value < 0)
+                {
+                    not1 = 0;
+                }
+                else if(value > 100)
+                {
+                    not1 = 100;
+                }
+                else
+                {
+                    not1 = value;
+                }
+            } 
+        }
+        public double Not2 { get => not2; set
+            {
+                if (value < 0)
+                {
+                    not2 = 0;
+                }
+                else if (value > 100)
+                {
+                    not2 = 100;
+                }
+                else
+                {
+                    not2 = value;
+                }
+            }
+        }
+        public double Not3 { get => not3; set
+            {
+                if (value < 0)
+                {
+                    not3 = 0;
+                }
+                else if (value > 100)
+                {
+                    not3 = 100;
+                }
+                else
+                {
+                    not3 = value;
+                }
+            }
+        }
+
+        public double OrtalamaHesap()
+        {
+            return (not1 + not2 + not3) / 3;
+        }
+
+        public double Ortalama { get { return OrtalamaHesap(); } } //read only
+    }
+}
