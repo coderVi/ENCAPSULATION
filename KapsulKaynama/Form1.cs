955fcce4723c8f0f70e6a1dabe9a19eaebb3bba9
Abdülsamet YILMAZ <82230018+coderVi@users.noreply.github.com>
2024-11-01 00:51:46 +0300

    Add files via upload

---
+++ KapsulKaynama/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KapsulKaynama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                Sicaklik sicaklik = new Sicaklik();
+            try
+            {
+                sicaklik.Isi = Convert.ToInt32(textBox1.Text);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Hatalı Giriş " + ex.Message);
+            }
+            MessageBox.Show(sicaklik.Isi.ToString());
+
+        }
+    }
+}
---
+++ KapsulKaynama/Sicaklik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsulKaynama
+{
+    internal class Sicaklik
+    {
+        int isi;
+        int basıc;
+
+        public int Isi {
+            get { return isi; }
+            set 
+            {
+                if (value <= 100 && value >= 0)
+                {
+                    isi = value;
+                }
+                else if (value < 0)
+                {
+                    isi = 0;
+                    throw new Exception("Fazla Soğuk");
+                }
+                else 
+                {
+                    isi = 100;
+                    throw new Exception("Fazla Sıcak");
+                }
+            } 
+        }
+    }
+}
